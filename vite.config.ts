17aa08a68195446c07bd959acd3924062e81c50d
Fadhiil Mahfudz <dilpadil28@gmail.com>
2025-09-25 19:02:25 +0700

    fix: base vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/github-explorer/',
+  base: '/github-explorer',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
